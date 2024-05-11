23f3698657328ae9c6766076dde3f725af0b3e6f
vincentmichelangeli <vincent@michelangeli.fr>
2024-05-11 14:59:31 +0100

    Function generating the data

--- Web_scraping/athlete_scraping.py
+++ Web_scraping/athlete_scraping.py
@@ -1,27 +1,33 @@
 from selenium import webdriver
-import requests
-from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
 
 
-driver_path = '/utils/chrome_driver/chromedriver.exe'
+
 driver = webdriver.Chrome()
-
-
-driver.get('https://worldathletics.org/world-rankings/road-running/women?regionType=world&page=1&rankDate=2024-05-05&limitByCountry=0')
-
-table = driver.find_element_by_class_name('records-table')
-buttons = table.find_element_by_tag_name("tr")
 
 data = []
 
-# Iterate over each button element
-for button in buttons:
-    # Find all <td> elements within each button
-    athlete_url = button.get_attribute('data-athlete-url')
-    tds = button.find_element_by_tag_name('td')
+def get_table(gender, events, n_pages):
+    data = []
+    for e in events:
+        for i in range(1,n_pages):
+            driver.get(f"https://worldathletics.org/world-rankings/{e}/{gender}?regionType=world&page={i}&rankDate=2024-05-05&limitByCountry=0")
+            table = driver.find_element(By.TAG_NAME, value = 'body')
+            table = table.find_element(By.CLASS_NAME , value='outer-container')
+            table = table.find_element(By.CLASS_NAME, value='site-container')
+            table = table.find_element(By.CLASS_NAME , value='records-table')
+            table = table.find_element(By.TAG_NAME , value='tbody')
+            buttons = table.find_elements(By.TAG_NAME, value="tr")
 
-    # Get data from each <td>
-    single_data = [td.text for td in tds]
-    single_data.append(athlete_url)
-    # Find the URL (assuming it's contained in an <a> tag within the first <td>)
-    data.append(single_data)
+            # Iterate over each button element
+            for button in buttons:
+                # Find all <td> elements within each button
+                athlete_url = "https://worldathletics.org" + button.get_attribute('data-athlete-url')
+                tds = button.find_elements(By.TAG_NAME,value='td')
+
+                # Get data from each <td>
+                single_data = [td.text for td in tds[1:4]]
+                single_data.append(athlete_url)
+                # Find the URL (assuming it's contained in an <a> tag within the first <td>)
+                data.append(single_data)
+    return set(data)
