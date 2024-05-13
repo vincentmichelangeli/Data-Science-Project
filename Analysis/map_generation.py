0b7f2eb74f942c2879bd745763891870f1d721e1
vincentmichelangeli <vincent@michelangeli.fr>
2024-05-13 22:12:03 +0100

    Adding density of athletes

--- Analysis/map_generation.py
+++ Analysis/map_generation.py
@@ -39,6 +39,8 @@ print(world)
 
 # Merge the country counts with the world map
 world = world.merge(country_counts, how='left', left_on='iso_a3', right_on='Nationality')
+world['Athletes_vs_pop'] = world['Count']/world['pop_est']*10000
+
 
 # Plot the map
 fig, ax = plt.subplots(1, 1, figsize=(15, 10))
@@ -77,6 +79,44 @@ os.makedirs(output_dir, exist_ok=True)
 
 # Save the figure
 output_path = os.path.join(output_dir, 'Athletes_countries.png')
+plt.savefig(output_path, bbox_inches='tight', dpi=300)
+
+plt.show()
+
+
+fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+
+xmin, xmax, ymin, ymax = -150, 160, -55, 75
+ax.set_xlim(xmin, xmax)
+ax.set_ylim(ymin, ymax)
+
+# Draw a frame around the continents
+frame = plt.Rectangle((xmin, ymin), xmax-xmin, ymax-ymin,
+                      linewidth=1, edgecolor='black', facecolor='lightblue')
+ax.add_patch(frame)
+
+world.plot(column='Athletes_vs_pop', ax=ax, legend=False,
+           edgecolor='black',  # Add a thin black outline on borders
+           linewidth=0.5,      # Set the outline thickness    
+           missing_kwds={"color": "lightgrey"})
+
+# Create a background within the frame
+ax.set_facecolor('lightblue')  # Set background color within the frame
+fig.patch.set_facecolor('white')  # Set background color for the figure
+
+
+# Add a vertical color bar
+sm = plt.cm.ScalarMappable(Normalize(vmin=world['Athletes_vs_pop'].min(), vmax=world['Athletes_vs_pop'].max()))
+sm._A = []  # Empty array for the ScalarMappable
+cbar = fig.colorbar(sm, ax=ax, orientation='vertical', pad=0.02, shrink=0.5)
+cbar.set_label('Density of Athletes by Country (/10000 person)', fontsize=12)
+
+# Remove the axis for a cleaner look
+ax.axis('off')
+
+
+# Save the figure
+output_path = os.path.join(output_dir, 'Athletes_countries_pop.png')
 plt.savefig(output_path, bbox_inches='tight', dpi=300)
 
 plt.show()
