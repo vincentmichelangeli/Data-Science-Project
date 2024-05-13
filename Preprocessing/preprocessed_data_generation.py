59d9657339265eb31235833a0f4540e21bd18428
vincentmichelangeli <vincent@michelangeli.fr>
2024-05-13 11:17:17 +0100

    creation of the generation function

---
+++ Preprocessing/preprocessed_data_generation.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from preprocessing_performances import process_races
+
+
+df_men_perf = pd.read_csv('\Data\Scraped_data\Men_performances.csv')
+new_records = []
+df_men_perf.apply(lambda row: new_records.extend(process_races(row)), axis=1)
+df_men_perf = pd.DataFrame(new_records, columns=['Name', 'Race', 'Year', 'Time'])
+
