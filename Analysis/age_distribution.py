ff5ec9acd895238156fa1e92c8f02f5d4ffb72a6
vincentmichelangeli <vincent@michelangeli.fr>
2024-05-14 15:36:59 +0100

    age distribution modif

--- Analysis/age_distribution.py
+++ Analysis/age_distribution.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from clustering import time_to_seconds
+import numpy as np
 
 races = ['1500 Metres', '5000 Metres', '10 Kilometres Road', '10,000 Metres','Half Marathon', 'Marathon']
 
@@ -18,7 +20,6 @@ def create_age_performance_table(Gender):
     # Count the number of performances for each age
     return performances_by_age
 
-print(create_age_performance_table('Men'))
 
 def plot_distribution(Gender):
     perf_by_age = create_age_performance_table(Gender)
@@ -30,4 +31,45 @@ def plot_distribution(Gender):
     plt.legend()
     plt.show()
 
+
+def calculate_time_differences(df, columns):
+    """Calculate the differences for successive years for a given column"""
+    diffs = []
+    previous_value = None
+    
+    for value in df[columns]:
+        if previous_value is not None:
+            diffs.append(value - previous_value)
+        else:
+            diffs.append(np.nan)
+        previous_value = value
+    
+    return diffs
+
+def plot_progression(Gender):
+    perf_by_age = create_age_performance_table(Gender)
+
+    perf_by_age.sort_values(by=['Name', 'Year'], inplace=True)
+
+    # Calculate the time differences between successive years for each athlete
+    progress_by_age = perf_by_age['Age']
+    progress_by_year = perf_by_age['Year']
+    for col in races:
+        perf_by_age[col] = perf_by_age[col].apply(time_to_seconds)
+        perf_by_age[col + '_diff'] = perf_by_age.groupby('Name')[col].diff()
+        progress_by_age[col + '_diff'] = perf_by_age[col + '_diff']
+        progress_by_year[col + '_diff'] = perf_by_age[col + '_diff']
+    print(progress_by_age)
+
+    progress_by_age = progress_by_age.groupby('Age').mean()
+    progress_by_year = progress_by_year.groupby('Year').mean()
+    plt.figure(figsize=(10,7))
+    for col in races:
+        plt.plot(progress_by_age[col + '_diff'], label = col)
+    plt.legend()
+    plt.show()
+
+
+
 plot_distribution('Men')
+plot_progression('Men')
